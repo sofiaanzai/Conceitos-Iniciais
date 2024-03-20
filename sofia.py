3ac41fbf3bc1d285f8f330964540055005547eed
sofiaanzai <160611455+sofiaanzai@users.noreply.github.com>
2024-03-20 09:15:43 -0300

    IDLE

---
+++ sofia.py
@@ -0,0 +1,61 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+2
+4
+5+2
+7
+2-3
+-1
+2+x=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+2+x==10
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    2+x==10
+NameError: name 'x' is not defined
+2x2
+SyntaxError: invalid decimal literal
+2.2
+2.2
+4+4/2
+6.0
+-2+4
+2
+-4+8
+4
+
+======================================================= RESTART: C:/Users/202102307104/Downloads/exemplo.py =======================================================
+Sofia
+
+======================================================= RESTART: C:/Users/202102307104/Downloads/exemplo.py =======================================================
+Sofia
+
+======================================================= RESTART: C:/Users/202102307104/Downloads/exemplo.py =======================================================
+Sofia
+nome
+'sofia'
+booleano
+True
+numero
+10
+numero + 10
+20
+numero * 100
+1000
+4//2
+2
+
+======================================================= RESTART: C:/Users/202102307104/Downloads/exemplo.py =======================================================
+Sofia
+a
+10
+b
+5
+soma
+15
+subtracao
+5
+multiplicacao
+50
+divisao
+2.0
