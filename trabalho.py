a89960e4ccae5b299024ee580c6974e42bf9cdc5
sofiaanzai <160611455+sofiaanzai@users.noreply.github.com>
2024-03-20 09:53:12 -0300

    Add files via upload

---
+++ trabalho.py
@@ -0,0 +1,36 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================================================== RESTART: C:/Users/202102307104/Downloads/sofia.py ========================================================
+
+======================================================== RESTART: C:/Users/202102307104/Downloads/sofia.py ========================================================
+a media é 5.0
+Traceback (most recent call last):
+  File "C:/Users/202102307104/Downloads/sofia.py", line 10, in <module>
+    print ("o quadrado é", quadrado)
+NameError: name 'quadrado' is not defined. Did you mean: 'quadradoa'?
+
+======================================================== RESTART: C:/Users/202102307104/Downloads/sofia.py ========================================================
+a media é 5.0
+Traceback (most recent call last):
+  File "C:/Users/202102307104/Downloads/sofia.py", line 10, in <module>
+    print ("o quadrado é", quadrado)
+NameError: name 'quadrado' is not defined. Did you mean: 'quadrado_a'?
+
+======================================================== RESTART: C:/Users/202102307104/Downloads/sofia.py ========================================================
+a media é 5.0
+o quadrado é 4
+o dobro é 12
+a quantidade de letras é 14
+a quantidade de espaços na frase é 3
+o b é maior que a.
+o maior numero é: 8
+
+======================================================== RESTART: C:/Users/202102307104/Downloads/sofia.py ========================================================
+a media é 5.0
+o quadrado é 4
+o dobro é 12
+a quantidade de letras é 14
+a quantidade de espaços na frase é 3
+o b é maior que a.
+o maior numero é: 8
