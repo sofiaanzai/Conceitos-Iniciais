8240108730a4ae903e2225492ff20d7aafa82597
sofiaanzai <160611455+sofiaanzai@users.noreply.github.com>
2024-03-27 10:20:59 -0300

    Add files via upload

---
+++ soso.py
@@ -0,0 +1,125 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+soma (6, 5)
+11
+
+def quadrado (a)
+SyntaxError: expected ':'
+def quadrado (a):
+    return a**2
+
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+hipotenusa (3, 4)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    hipotenusa (3, 4)
+NameError: name 'hipotenusa' is not defined. Did you mean: 'hiotenusa'?
+
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+hipotenusa (3, 4)
+5.0
+
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+simples(azul)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    simples(azul)
+NameError: name 'azul' is not defined
+simples(cor)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    simples(cor)
+NameError: name 'cor' is not defined. Did you mean: 'chr'?
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+simples('azul')
+'escolheu certo'
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+medio('azul')
+'escolheu certo'
+medio('roxo')
+'tente outra cor'
+medio('preto')
+'tente outra cor'
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+completo('preto')
+'não tem salvação'
+completo('azul')
+'escolheu certo'
+completo('branco')
+'tente outra cor'
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+completo('verde')
+'sorte'
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+Traceback (most recent call last):
+  File "C:/Users/202103835571/Desktop/lelen.py", line 33, in <module>
+    print(numero[0])
+NameError: name 'numero' is not defined. Did you mean: 'numeros'?
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+Traceback (most recent call last):
+  File "C:/Users/202103835571/Desktop/lelen.py", line 33, in <module>
+    print(numero[0])
+NameError: name 'numero' is not defined. Did you mean: 'numeros'?
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+1
+5
+[10, 2, 3, 4, 5]
+
+1
+1
+
+========================================================= RESTART: C:/Users/202103835571/Desktop/lelen.py =========================================================
+1
+5
+[10, 2, 3, 4, 5]
+=== começando o while ===
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+=== começando o for com range (10) ===
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+1
+45
+78
+a
+[3, 5]
+m
+i
+n
+h
+a
+ 
+s
+t
+r
+i
+n
+g
